e097df147aa99c7ebda4566a168f9fce4f21d2aa
Frank Hmeidan <frank.hmeidan@gmail.com>
2014-01-25 13:00:04 -0500

    added redundant app.js for heroku

---
+++ app/app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+// Serve everything from public.
+app.use(express.static(__dirname + '/../public'));
+
+app.use(function(req, res){
+  res.sendfile(__dirname + "/../public/index.html");
+});
+
+var port = Number(process.env.PORT || 3000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
